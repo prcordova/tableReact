f9737e623c09c0d22d83433dbe5bc59d403312a3
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2021-10-07 03:57:05 -0300

    table updated

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
@@ -11,11 +11,11 @@ import Footer from '../components/template/Footer'
 
 
 export default props =>
-  <HashRouter>
+  <BrowserRouter>
     <div className="app">
       <Logo />
       <Nav />
       <Routes />
       <Footer />
     </div >
-  </HashRouter>
+  </BrowserRouter>
