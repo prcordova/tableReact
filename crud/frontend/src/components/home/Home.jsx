aa21e0749e62a832a47f6183a61da5ee7878e043
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2021-10-07 03:10:15 -0300

    routes, layout, user and home page components created

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../template/Main';
+
+export default props =>
+  <Main icon="home" title="Inicio"
+    subtitle="Segundo Projeto do capitulo de React." >
+    <div className="display-4">Bem vindo!</div>
+    <hr />
+    <p className="mb-0">
+      Sistema para exemplificar a construção de um cadastro de usuário desenvolvido em react
+    </p>
+  </Main>
---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+  <footer className="footer">
+    <span>Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Proc
+      <span className="text-danger">Labs</span></strong></span>
+  </footer>
